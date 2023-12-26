c3ea776cc2c65d0876ae442b97288aa3ae1071ff
Gurpreet Singh <gurpreet012guru@gmail.com>
2023-12-26 19:52:54 +0530

    add tooltip

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,25 +1,29 @@
 import React from "react";
 import styles from "./Card.module.css";
+import Tooltip from "@mui/material/Tooltip";
 const Card = ({ data, type }) => {
-	// console.log(data);
+	console.log(data);
+	const title = data.songs ? data.songs.length + " songs" : null;
 	return (
-		<div className={styles.wrapper}>
-			<div className={styles.card}>
-				<img src={data.image} alt="song" loading="lazy" />
-				<div className={styles.banner}>
-					<div id={styles.pill}>
-						{type === "song" ? (
-							<p>{data.likes} Likes</p>
-						) : (
-							<p>{data.follows} Follows</p>
-						)}
+		<Tooltip title={title} arrow>
+			<div className={styles.wrapper}>
+				<div className={styles.card}>
+					<img src={data.image} alt="song" loading="lazy" />
+					<div className={styles.banner}>
+						<div id={styles.pill}>
+							{type === "song" ? (
+								<p>{data.likes} Likes</p>
+							) : (
+								<p>{data.follows} Follows</p>
+							)}
+						</div>
 					</div>
 				</div>
+				<div className={styles.titleWrapper}>
+					<p>{data.title}</p>
+				</div>
 			</div>
-			<div className={styles.titleWrapper}>
-				<p>{data.title}</p>
-			</div>
-		</div>
+		</Tooltip>
 	);
 };
 
